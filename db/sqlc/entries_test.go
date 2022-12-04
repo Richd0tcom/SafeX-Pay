df14dacec793861f62be454b0d09959817059388
Richd0tcom <tuberich0@gmail.com>
2022-12-05 00:44:14 +0100

    test(entries): write unit tests for entries queries

--- db/sqlc/entries_test.go
+++ db/sqlc/entries_test.go
@@ -1,2 +1,79 @@
 package db
 
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/Richd0tcom/SafeX-Pay/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T, account Account) Entry{
+	args:= CreateEntryParams{
+		AccountID: account.ID,
+		Amount: utils.RandomMoney(),
+	}
+
+	entry, err :=testQueries.CreateEntry(context.Background(), args)
+	
+	//check that there are no errors
+	require.NoError(t,err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, args.AccountID, entry.AccountID)
+	require.Equal(t, args.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T){
+	account:= createRandomAccount(t)
+
+	createRandomEntry(t, account)
+}
+
+func TestGetEntry(t *testing.T){
+	//create a new account
+	account:= createRandomAccount(t)
+
+	//create a new entry for that account
+	entry1:= createRandomEntry(t, account)
+
+	entry2, err:=testQueries.GetEntry(context.Background(), entry1.ID)
+
+	//check that there is no error
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	//check that the values are valid
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestListEntries(t *testing.T) {
+	account := createRandomAccount(t)
+	for i := 0; i < 10; i++ {
+		createRandomEntry(t, account)
+	}
+
+	arg := ListEntriesParams{
+		AccountID: account.ID,
+		Limit:     5,
+		Offset:    5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+		require.Equal(t, arg.AccountID, entry.AccountID)
+	}
+}
