b4b3b594ed1aad06e690e31a47966a6ac153436c
Richd0tcom <tuberich0@gmail.com>
2022-12-05 00:08:49 +0100

    test(accounts): write unit test for account queries

--- db/sqlc/accounts_test.go
+++ db/sqlc/accounts_test.go
@@ -3,12 +3,14 @@ package db
 import (
 	"context"
 	"testing"
-
-	"github.com/stretchr/testify/require"
+	"time"
+	"database/sql"
 	"github.com/Richd0tcom/SafeX-Pay/utils"
+	"github.com/stretchr/testify/require"
 )
- 
-func TestCreateAccounts(t *testing.T){
+
+//create a random account to make tests independent
+func createRandomAccount(t *testing.T) Account{
 	//instantiates a new account object(struct)
 	args := CreateAccountParams{
 		Owner: utils.RandomOwner(),
@@ -33,29 +35,72 @@ func TestCreateAccounts(t *testing.T){
 	require.NotZero(t,account.ID)
 	require.NotZero(t,account.CreatedAt)
 
+	return account
 }
+func TestCreateAccounts(t *testing.T){
+	createRandomAccount(t)
+}
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
 
-func TestListAccounts(t *testing.T){
-	args := ListAccountsParams{
-		Limit:1 ,
-		Offset:1,
-	}
-	accounts, err := testQueries.ListAccounts(context.Background(),args)
-	//checks that the CreateAccount function returns NO error. will fail the test if error exists
+	//check that there are no errors
 	require.NoError(t, err)
-	//checks that the new account created should not be empty.
-	require.NotEmpty(t,accounts)
+	require.NotEmpty(t, account2)
 
-	//checks that the values of the account object are of the sam type.
-	require.Equal(t, args.Owner, account.Owner)
-	require.Equal(t, args.Balance, account.Balance)
-	require.Equal(t, args.Currency, account.Currency)
-	require.
+	//check that the returned data is valid
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+func TestListAccounts(t *testing.T){
+	for i := 0; i < 10; i++ {
+		createRandomAccount(t)
+	}
 
-	//checks that the values of the Timestamp is not its zero value i.e it has been created
-	require.NotZero(t,account.ID)
-	require.NotZero(t,account.CreatedAt)
+	arg := ListAccountsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, accounts)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
 
 }
-func TestUpdateAccount(t *testing.T){}
-func TestDeleteAccount(){}
+func TestUpdateAccount(t *testing.T){
+	account1 := createRandomAccount(t)
+
+	arg := UpdateAccountParams{
+		ID:      account1.ID,
+		Balance: utils.RandomMoney(),
+	}
+
+	account2, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, arg.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+func TestDeleteAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	//try to retrieve deleted account. should throw an error
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, account2)
+}
--- db/sqlc/entries_test.go
+++ db/sqlc/entries_test.go
@@ -1,13 +1,2 @@
 package db
 
-import (
-	"context"
-	"testing"
-
-	"github.com/stretchr/testify/require"
-	"github.com/Richd0tcom/SafeX-Pay/utils"
-)
-
-func TestCreateEntry(t *testing.T){
-	
-}
