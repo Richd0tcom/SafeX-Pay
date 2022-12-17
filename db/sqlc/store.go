204dfca70d51dea6c0c550e11b3c68b5fbc55602
Richd0tcom <tuberich0@gmail.com>
2022-12-17 12:37:18 +0100

    feat(transfers): implement transfers db transactions

---
+++ db/sqlc/store.go
@@ -0,0 +1,95 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+//provides all functions to execute queries and transactions.
+//we will use composition instead of inheritance by mbedding the *Queries struct in the Store
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+//NewStore creates a new Store
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db: db,
+		Queries: New(db),
+	}
+}
+
+//Takes a context and a callback function as input, starts a new database transaction, creat a new Queries object and with that transaction and call the callback function with the created Queries object and finally commit or rollback the transaction based on the error returned by the callback function.
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error{
+	tx, err :=store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	querysObject := New(tx)
+	txErr := fn(querysObject)
+	if txErr != nil {
+		rbErr:= tx.Rollback()
+		if rbErr != nil {
+			//this means that the rollback failed 
+			return fmt.Errorf("transaction Error: %v, Rollback Error: %v", txErr, rbErr)
+		}
+		return txErr //IF the rollback is successful, return the originall transaction error
+	}
+	//If all operations are successful, commit the transaction
+	return tx.Commit()
+}
+
+//TransferTx perform a money transfer from one account to another account
+func (store Store) TransferTx(ctx context.Context, args TransferTxParams) (TransferTxResults, error) {
+	var result TransferTxResults
+
+	err:= store.execTx(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx,CreateTransferParams{
+			FromAccountID: args.FromAccountID,
+			ToAccountID: args.ToAccountID,
+			Amount: args.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: args.FromAccountID,
+			Amount: -args.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: args.ToAccountID,
+			Amount: args.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		//TODO: UPDATE account balance info
+		return nil
+	})
+
+	return result, err
+}
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID int64 `json:"to_account_id"`
+	Amount int64 `json:"amount"`
+}
+
+type TransferTxResults struct {
+	Transfer Transfer `json:"transfer"`
+	FromAccount Account `json:"from_account"`
+	ToAccount Account `json:"to_account"`
+	FromEntry Entry `json:"from_entry"`
+	ToEntry Entry `json:"to_entry"`
+}
---
+++ db/sqlc/store_test.go
@@ -0,0 +1,59 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransferTx(t *testing.T){
+	store:=NewStore(testDB)
+	account1:= createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	amount:= int64(10)
+
+	//TODO: use concurrency to handle multiple transfers
+	results, err :=store.TransferTx(context.Background(), TransferTxParams{
+		FromAccountID: account1.ID,
+		ToAccountID: account2.ID,
+		Amount: amount,
+	})
+
+	require.NoError(t,err)
+	require.NotEmpty(t,results)
+
+	// check transfer
+	transfer := results.Transfer
+	require.NotEmpty(t, transfer)
+	require.Equal(t, account1.ID, transfer.FromAccountID)
+	require.Equal(t, account2.ID, transfer.ToAccountID)
+	require.Equal(t, amount, transfer.Amount)
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+
+	_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
+		// check entries
+		fromEntry := results.FromEntry
+		require.NotEmpty(t, fromEntry)
+		require.Equal(t, account1.ID, fromEntry.AccountID)
+		require.Equal(t, -amount, fromEntry.Amount)
+		require.NotZero(t, fromEntry.ID)
+		require.NotZero(t, fromEntry.CreatedAt)
+	
+		_, err = store.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+
+		toEntry := results.ToEntry
+		require.NotEmpty(t, toEntry)
+		require.Equal(t, account2.ID, toEntry.AccountID)
+		require.Equal(t, amount, toEntry.Amount)
+		require.NotZero(t, toEntry.ID)
+		require.NotZero(t, toEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), toEntry.ID)
+		require.NoError(t, err)
+}
